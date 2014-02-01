a0c7590eab9e655b31a8426ca7c2ff55dab8365b
jgdimeglio <jgdimeglio@gmail.com>
2014-02-01 05:55:43 -0300

    Se agregó la grilla con todos sus recursos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var grilla = require('./routes/grilla/index');
 var http = require('http');
 var path = require('path');
 
@@ -31,6 +32,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/grilla', grilla.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ models/datos.js
@@ -0,0 +1,5 @@
+var datos = require('./initial_data.json');
+
+ 
+//de esta forma exportamos el objeto
+module.exports = datos;
