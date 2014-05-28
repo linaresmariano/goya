b48f30b117b2cb4a509c658ddb0514d25d5bfe49
unknown <jgdimeglio@gmail.com>
2014-05-28 22:52:18 +0200

    agregando relacion  entre curso y horario

--- models/course.js
+++ models/course.js
@@ -1,12 +1,18 @@
+
 module.exports = function(sequelize, DataTypes) {
 
   var Course = sequelize.define('Course', {
     code: DataTypes.STRING,
 	commission: DataTypes.INTEGER,
 	semester: DataTypes.INTEGER,
-	enrolled: DataTypes.INTEGER,
-  }, {
+	enrolled: DataTypes.INTEGER
+    }, {
+    classMethods: {
+      associate: function(models) {
+        Course.hasMany(models.CourseSchedule,{ as: 'Schedules'})
+      }
+    }
   })
- 
+
   return Course;
 }
