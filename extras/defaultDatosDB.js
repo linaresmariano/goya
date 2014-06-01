1e0d2a504b15293d6cc331b7db975506dab0b10d
Mariano <marianolinares92@gmail.com>
2014-06-01 11:04:10 -0300

    Default datos

--- extras/defaultDatosDB.js
+++ extras/defaultDatosDB.js
@@ -7,88 +7,28 @@ db.Course.destroy().success(function() {
 		code: 'EPERS',
 		commission: 1,
 		semester: 12014,
-		enrolled: 23
+		enrolled: 25
+	})
+
+	db.Course.create({
+		code: 'INTRO',
+		commission: 2,
+		semester: 12014,
+		enrolled: 30
+	})
+
+	db.Course.create({
+		code: 'ORGA',
+		commission: 4,
+		semester: 12014,
+		enrolled: 45
+	})
+
+	db.Course.create({
+		code: 'SEGI',
+		commission: 1,
+		semester: 12014,
+		enrolled: 20
 	})
 
 })
-
-// curso = new Curso();
-// 	  curso.code= 'EPERS';
-// 	  curso.comision = 1;
-// 	  curso.cuatrimestre = 12014;
-// 	  curso.inscriptos = 25;
-// 	  curso.configVista.color='red';
-// 	  curso.requisitos.computadoras =true;
-// 	  curso.horarios=[{
-// 				dia : 1,
-// 				hora: 16,
-// 				minutos: 0,
-// 				duracion: 6,
-// 				tipo: 'Teorica/Practica'
-// 			}]
-// curso.save();
-
-// curso= new Curso();
-// 	  curso.code= 'INTRO';
-// 	  curso.comision = 2;
-// 	  curso.cuatrimestre = 12014;
-// 	  curso.inscriptos = 30;
-// 	  curso.configVista.color='pink';
-// 	  curso.requisitos.computadoras =true;
-// 	  curso.horarios=[{
-// 				dia : 1,
-// 				hora: 9,
-// 				minutos: 0,
-// 				duracion: 3,
-// 				tipo: 'Teorica'
-// 			},
-// 			{
-// 				dia : 3,
-// 				hora: 9,
-// 				minutos: 0,
-// 				duracion: 3,
-// 				tipo: 'Practica'
-// 			},
-// 			{
-// 				dia : -1,
-// 				hora: -1,
-// 				minutos: 0,
-// 				duracion: 3,
-// 				tipo: 'Practica'
-// 			}
-// 			]
-// curso.save();
-
-
-// curso= new Curso();
-// 	  curso.code= 'ORGA';
-// 	  curso.comision = 4;
-// 	  curso.cuatrimestre = 12014;
-// 	  curso.inscriptos = 45;
-// 	  curso.configVista.color='green';
-// 	  curso.requisitos.computadoras =true;
-// 	  curso.horarios=[{
-// 				dia : 5,
-// 				hora: 14,
-// 				minutos: 0,
-// 				duracion: 4,
-// 				tipo: 'Teorica'
-// 		         }]
-
-// curso.save();
-				 
-// curso= new Curso();
-// 	  curso.code= 'SEGI';
-// 	  curso.comision = 1;
-// 	  curso.cuatrimestre = 12014;
-// 	  curso.inscriptos = 20;
-// 	  curso.configVista.color='yellow';
-// 	  curso.requisitos.computadoras =true;
-// 	  curso.horarios=[{
-// 				dia : -1,
-// 				hora: -1,
-// 				minutos: 0,
-// 				duracion: 4,
-// 				tipo: 'Teorica/Practica'
-// 			 }]
-// curso.save();
