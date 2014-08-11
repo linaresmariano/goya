a6338908f33bf86760551e8d2c327cbe4b534c4e
unknown <jgdimeglio@gmail.com>
2014-08-11 23:35:46 +0200

    mostrando los semestres ordenados

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ var db = require('../models')
 
 exports.index = function(req, res){
 
-  db.Semester.findAll().success(function(semesters) {
+  db.Semester.findAll({
+		include: [ {	model: db.Teacher, as: 'Teachers' ,require:false}],
+		order:' year DESC ,semester DESC'
+	}).success(function(semesters) {
     res.render('index', {
       title: 'Semestres',
       semesters: semesters
