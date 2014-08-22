89a93c45aa1bbd44cba2210e3f59bbfbd816ba0a
unknown <jgdimeglio@gmail.com>
2014-08-22 15:53:28 +0200

    asignar profesor en el modelo

--- models/courseSchedule.js
+++ models/courseSchedule.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
 
   return sequelize.define('CourseSchedule', {
     type: DataTypes.STRING,
-  	day: {type:DataTypes.INTEGER ,  validate: {min:0,max:6}},
+  	day: {type:DataTypes.INTEGER ,  validate: {min:-1,max:6}},
   	hour: {type:DataTypes.INTEGER ,  validate: {min:-1,max:22}},
   	minutes: DataTypes.INTEGER,
   	duration: {type:DataTypes.INTEGER ,  validate: {min:0,max:6}}
@@ -21,7 +21,44 @@ module.exports = function(sequelize, DataTypes) {
 						})
 				})
 		}
-      }
+      },
+	  instanceMethods:{
+		assignedTeacher:function(db,idTeacher,semester,year,succes){
+			var schedule=this;
+			db.SemesterTeacher.find({
+			where: {'Teacher.id':idTeacher,'Semester.year':year,'Semester.semester':semester},
+			include: [ {	model: db.Teacher, as: 'Teacher' ,require:false },
+					{	model: db.Semester, as: 'Semester' ,require:false }]
+			}
+			).success(function(semesterTeacher) {
+		
+				if(semesterTeacher == undefined){
+					
+					db.Teacher.find(idTeacher).success(function(teacher) {
+						var newSemesterTeacher= db.SemesterTeacher.create({
+						}).success(function(newSemesterTeacher) {
+											newSemesterTeacher.setTeacher(teacher);
+											schedule.addSemesterTeacher(newSemesterTeacher);	
+											db.Semester.find({where: {'year':year,'semester':semester}}).success(function(semester) {
+												semester.addSemesterTeacher(newSemesterTeacher);
+												succes();
+											});
+																					
+									});
+					
+					})
+					console.log('el profesor es nulo');
+				}else{
+					console.log('el profesor no es nulo');
+					schedule.addSemesterTeacher(semesterTeacher);	
+					succes();
+
+				}
+			
+			
+			})
+		}
+	  }
   })
 
 }
--- routes/schedule/index.js
+++ routes/schedule/index.js
@@ -9,42 +9,10 @@ exports.assignedTeacher = function(req, res) {
     var idCourseSchedule = req.body.idCourseSchedule;
   	var year = req.body.year;
     var semester = req.body.semester;
-  db.CourseSchedule.find(idCourseSchedule).success(function(courseSchedule) {
-
-		db.SemesterTeacher.find({
-			where: {'Teacher.id':idTeacher,'Semester.year':year,'Semester.semester':semester},
-			include: [ {	model: db.Teacher, as: 'Teacher' ,require:false },
-					{	model: db.Semester, as: 'Semester' ,require:false }]
-			}
-		).success(function(semesterTeacher) {
-		
-			if(semesterTeacher == undefined){
-				
-				db.Teacher.find(idTeacher).success(function(teacher) {
-					var newSemesterTeacher= db.SemesterTeacher.create({
-					}).success(function(newSemesterTeacher) {
-										newSemesterTeacher.setTeacher(teacher);
-										courseSchedule.addSemesterTeacher(newSemesterTeacher);	
-										db.Semester.find({where: {'year':year,'semester':semester}}).success(function(semester) {
-											semester.addSemesterTeacher(newSemesterTeacher);
-											res.send('ok')
-										});
-																				
-								});
-				
-				})
-				console.log('el profesor es nulo');
-			}else{
-				console.log('el profesor no es nulo');
-				courseSchedule.addSemesterTeacher(semesterTeacher);	
-				res.send('ok')
-
-			}
-			
-			
-		})
-  
-
+	db.CourseSchedule.find(idCourseSchedule).success(function(courseSchedule) {
+		courseSchedule.assignedTeacher(db,idTeacher,semester,year,function(){
+			res.send('ok');
+		});
   })
 }
 
