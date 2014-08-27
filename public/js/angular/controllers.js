ff2355d0bcb28b26bf2ab8af10b3262f823f19d2
linaresmariano <marianolinares92@gmail.com>
2014-08-27 11:28:03 -0300

    Acomodando el controller del layout

--- public/js/angular/controllers.js
+++ public/js/angular/controllers.js
@@ -1,5 +1,28 @@
+var app = angular.module('APP', ['LocalStorageModule','ui.calendar', 'ui.bootstrap']);
 
-app.controller('SemestralGrids', function ($scope,localStorageService, $location) {
+
+app.controller('WebController', function ($scope, localStorageService, $http) {
+	if(localStorageService.get('year') == undefined && localStorageService.get('number') == undefined) {
+		$http({
+				url:"/lastSemester",
+				method:'get',
+				data: {}
+		}).success(function(data) {
+			$scope.year=data.semester.year
+			$scope.number=data.semester.number
+			localStorageService.set('year', $scope.year)
+			localStorageService.set('number', $scope.number)
+		}).error(function(err) {
+			alert('Error al conectar con el servidor.')
+		})
+	} else {
+		$scope.year = localStorageService.get('year')
+		$scope.number = localStorageService.get('number')
+	}
+})
+
+
+app.controller('SemestralGrids', function ($scope, localStorageService, $location) {
   $scope.semesters = semestersJSON;
   $scope.currentSemester=function(year,number){
 			localStorageService.set('year',year);
