d1a080e3e25ed1add0c0a04de424c085ff61bf3e
linaresmariano <marianolinares92@gmail.com>
2014-08-27 10:59:58 -0300

    Guardar id del core, modality y area

--- public/js/angular/controllers.js
+++ public/js/angular/controllers.js
@@ -8,3 +8,30 @@ app.controller('SemestralGrids', function ($scope,localStorageService, $location
 	}
   
 })
+
+
+app.controller('newSubjectCtrl', function ($scope, localStorageService) {
+
+  $scope.areas = [
+    {id:0, name:'Programación'},
+    {id:1, name:'Sistemas Informáticos'},
+    {id:2, name:'Matemática Básica'},
+    {id:3, name:'Matemática Superior'},
+    {id:4, name:'Idioma'}
+  ]
+
+  $scope.cores = [
+    {id:0, name:'Básico Obligatorio'},
+    {id:1, name:'Avanzado Obligatorio'},
+    {id:2, name:'Complementaria'},
+    {id:3, name:'Obligatoria'}
+  ]
+
+  $scope.modalities = [
+    {id:0, name:'Presencial'},
+    {id:1, name:'Presencial con campus virtual'},
+    {id:2, name:'Virtual'}
+  ]
+  
+})
+
