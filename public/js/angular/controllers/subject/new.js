48cf6b6e2d55a62a252b630e7965dd7f1eb62a66
Mariano <marianolinares92@gmail.com>
2014-10-04 13:20:51 -0300

    Template para tener un controller por pagina

---
+++ public/js/angular/controllers/subject/new.js
@@ -0,0 +1,31 @@
+
+// app.controller('newSubjectCtrl', function ($scope, localStorageService, subjectService) {
+
+//   $scope.areas = subjectService.areas
+
+//   $scope.cores = subjectService.cores
+
+//   $scope.dictateCareers = []
+
+//   $scope.addDictate = function(dictate){
+//     if(dictate != null && !isRepeat(dictate)) {
+//       $scope.dictateCareers.push(dictate)
+//     }
+//   }
+
+//   function isRepeat(dictate){
+//     for(i =0; i < $scope.dictateCareers.length; i++) {
+//       var career = $scope.dictateCareers[i]
+//       if(career.id == dictate.id) {
+//         return true
+//       }
+//     }
+
+//     return false
+//   }
+
+//   $scope.remove = function(index) {
+//     $scope.dictateCareers.splice(index, 1)
+//   }
+  
+// })
