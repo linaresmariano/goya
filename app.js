fc5fa03a5d9b0803e645910853120795d7d26f5d
Mariano <marianolinares92@gmail.com>
2014-10-26 16:11:10 -0300

    usar bootstrap de bower

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(bodyParser());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower', express.static(__dirname + '/bower_components'));
 
 
 // development only
