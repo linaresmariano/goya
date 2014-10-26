2f372547d67d609d65875fae8716e79a9d9195cf
Mariano <marianolinares92@gmail.com>
2014-10-26 10:04:19 -0300

    Validar que no haya sido creado el semestre previamente

--- routes/semester.js
+++ routes/semester.js
@@ -22,17 +22,32 @@ exports.create = function(req, res) {
   var semester = req.body.semester;
   var year = req.body.year;
 
-  db.Semester.create({
-    year: year,
-    semester: semester
-  }).success(function(teacher) {
-    req.flash(typeMessage.SUCCESS, 'Semestre creado correctamente');
-    res.render('semester/new', {
-      title: 'Crear un semester',
-    })
-  }).error(function(err) {
-    showErrors(req,err);
-    res.redirect('back');
+  db.Semester.find({
+    where: {'year': year, 'semester': semester}
+  }).success(function(semesters) {
+
+    if(!semesters) {
+
+      db.Semester.create({
+        year: year,
+        semester: semester
+      }).success(function(teacher) {
+
+        res.redirect('/')
+        req.flash(typeMessage.SUCCESS, "El semestre se ha guardado correctamente")
+
+      }).error(function(err) {
+        showErrors(req, err);
+        res.redirect('back');
+      })
+
+    } else {
+
+      req.flash(typeMessage.ERROR, "El semestre "+semester+" del año "+year+" ya existe")
+      res.redirect('back');
+
+    }
+
   })
 
 }
