1badbe0b8a84a9f6e641504f337dbf8a63f276dd
unknown <jgdimeglio@gmail.com>
2014-11-01 01:30:23 +0100

    query para validad la superposicion de aulas

--- models/classroom.js
+++ models/classroom.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
 
-  return sequelize.define('ClassRoom', {
+  var ClassRoom= sequelize.define('ClassRoom', {
 	name:{type:DataTypes.STRING,
       validate: {
         len: {
@@ -37,8 +37,63 @@ module.exports = function(sequelize, DataTypes) {
   },{classMethods: {
 			associate: function(models) {
 			  this.hasMany(models.SemesterClassRoom, { as: 'SemesterClassRoom'});
+			},
+			
+			checkClassroomUsed: function(idClassRoom,schedule,year,semester,success) {
+			  	var CourseSchedule=ClassRoom.models.CourseSchedule;
+				var Semester=ClassRoom.models.Semester;
+				var SemesterClassRoom=ClassRoom.models.SemesterClassRoom;
+				
+				sequelize.query("SELECT * FROM "+ClassRoom.tableName
+								+" JOIN "+SemesterClassRoom.tableName
+										+" ON "+SemesterClassRoom.tableName+".ClassRoomId = "
+										+ClassRoom.tableName+".id "
+								+" JOIN "+CourseSchedule.tableName
+										+" ON "+CourseSchedule.tableName+".SemesterClassRoomId = "
+										+SemesterClassRoom.tableName+".id "
+								+" JOIN "+Semester.tableName
+										+" ON "+Semester.tableName+".id = "
+										+SemesterClassRoom.tableName+".semesterId "
+								+" WHERE "+ClassRoom.tableName+".id = "+idClassRoom
+										+" AND "+Semester.tableName+".year = "+year
+										+" AND "+Semester.tableName+".semester = "+semester
+										+" AND ((("+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes < "
+											+(schedule.hour*60+schedule.minutes )
+										+" AND "+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes +"
+											+CourseSchedule.tableName+".durationHour *60 + "+CourseSchedule.tableName+".durationMinutes > "
+											+(schedule.hour*60+schedule.minutes)
+										+") OR ("+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes < "
+											+(schedule.durationHour*60+schedule.durationMinutes+schedule.hour*60+schedule.minutes )
+										+" AND "+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes +"
+											+CourseSchedule.tableName+".durationHour*60 + "+CourseSchedule.tableName+".durationMinutes > "
+											+(schedule.durationHour*60+schedule.durationMinutes+schedule.hour*60+schedule.minutes)
+										+") AND "+CourseSchedule.tableName+".day = "+schedule.day+" ) "
+										+" OR (("+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes = "
+											+(schedule.hour*60+schedule.minutes )
+										+" AND "+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes +"
+											+CourseSchedule.tableName+".durationHour *60 + "+CourseSchedule.tableName+".durationMinutes = "
+											+(schedule.hour*60+schedule.minutes)
+										+") AND ("+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes = "
+											+(schedule.durationHour*60+schedule.durationMinutes+schedule.hour*60+schedule.minutes )
+										+" AND "+CourseSchedule.tableName+".hour*60 + "+CourseSchedule.tableName+".minutes +"
+											+CourseSchedule.tableName+".durationHour*60 + "+CourseSchedule.tableName+".durationMinutes = "
+											+(schedule.durationHour*60+schedule.durationMinutes+schedule.hour*60+schedule.minutes)
+										+") AND "+CourseSchedule.tableName+".day = "+schedule.day+" ))" ).success(function(myTableRows) {
+								
+				    if(myTableRows.length > 0){
+						console.log(myTableRows);
+						console.log(schedule);
+						success({error:'El aulas esta siendo usada en ese horario!'});
+					}else{
+						success();
+					}
+					
+				}).error(function(err) {
+					console.log(err);
+				})
+
 			}
 		}
     })
-  
+	return ClassRoom;
 }
