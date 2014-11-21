768941b9120b876e769c5eb8eade36474d10f0ce
unknown <jgdimeglio@gmail.com>
2014-11-21 18:47:29 +0100

    test para la asignacion de aulas

---
+++ tests/classroomTest.js
@@ -0,0 +1,20 @@
+var db = require('./');
+var  sequelize= db.sequelize;
+
+
+exports.testCheckClassroomUsedOk = function (test) {
+	idClassRoom=1;
+	year=2014;
+	semester=1;
+	idCourseSchedule=1;
+	db.CourseSchedule.find({where: {'id':idCourseSchedule},
+			  include: [{model: db.PatchSchedule, as: 'Patch', require: false}]}).success(function(schedule){
+					db.ClassRoom.checkClassroomUsed(idClassRoom,schedule,year,semester,function(msj){
+
+						test.equal(msj,undefined,"msj  should be undefined" );
+						test.done();
+					});		
+	});	
+}
+
+
